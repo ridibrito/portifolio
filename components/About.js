e0e7d5e6a38e3b229bee147c0be17aa098fb9b90
ridibrito <devoldbsb@gmail.com>
2022-08-31 08:46:27 -0300

    quote

--- components/About.js
+++ components/About.js
@@ -9,9 +9,9 @@ export default function About() {
           Sobre mim
         </h2>
         <h2 className="text-xl font-semibold"> Eu sou Ricardo!</h2>
-        <p>Nunca fui o 'cara' da tecnologia, longe disso,sempre fui o comercial, comunicativo, descontraido e socialmente engajado. Aquele que conhece 'todo mundo', brincalhão...</p>
+        <p>Nunca fui o <q>cara</q>  da tecnologia, longe disso,sempre fui o comercial, comunicativo, descontraido e socialmente engajado. Aquele que conhece <q>todo mundo</q>, brincalhão...</p>
         <p>No entanto, sempre fui dono de muitas idéias, ajudando a mudar positivamente cada lugar por onde passei. Idéias que sempre surgiram por um inconformismo de quem pensa fora da caixa.</p>
-        <p>Em um desses processos de não me conformar com explicações de profissionais de tecnologia de que "isso não dá pra fazer", "aquilo lá" leva muito tempo e dinheiro pra fazer e etc... DECIDI APRENDER E ME APAIXONEI, por design, por tecnologia, por cada desafio que este caminho me trouxe.</p>
+        <p>Em um desses processos de não me conformar com explicações de profissionais de tecnologia de que <q>isso não dá pra fazer</q>, <q>aquilo lá</q> leva muito tempo e dinheiro pra fazer e etc... DECIDI APRENDER E ME APAIXONEI, por design, por tecnologia, por cada desafio que este caminho me trouxe.</p>
         <p>...a história é longa, mas posso te contar tomando uma boa xícara de café.</p>
         <h3 className="text-lg font-semibold">Hoje minha vida é na tranquila e ensolarada Caldas Novas -GO...a terra das águas termais! 😉</h3>
         
